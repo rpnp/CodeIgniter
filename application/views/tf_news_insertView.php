a7cb691976b7ea9e4552ba01458160c44fa96c01
rpnp_hua <zxh3389@gmail.com>
2013-07-28 17:56:44 +0800

    视图修改
    界面修改，upload图片删除，增加css文件

--- application/views/tf_news_insertView.php
+++ application/views/tf_news_insertView.php
@@ -1,12 +1,28 @@
 <html>
+<head>
+<link rel='stylesheet' media="screen" type='text/css' href='/CodeIgniter/css/views.css' />
+</head>
+<body>
+<div id="bigBox">
+	<div id="left">
+        <ul>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/newsList">首页</a></li>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/newsList">新闻列表</a></li>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/insertView">添加新闻</a></li>
+        </ul>
+    </div>
+    <div id="right">
+
 <form action="/CodeIgniter/index.php/tf_news_c/insert" method="post">
 <table>
 <tr><td>标题</td>
-	<td><input name="title" type="text" /></td><tr>
+	<td><input name="title" type="text" size="30" /></td><tr>
 <tr><td valign="top">内容</td>
-	<td><textarea name="content" cols="60" rows="12"></textarea></td><tr>
+	<td><textarea name="content" cols="70" rows="7"></textarea></td><tr>
 <tr><td>&nbsp;</td>
-	<td><input name="sub" type="submit" value="提交" /></td></tr>
+	<td><input class="button" name="sub" type="submit" value="提交" /></td></tr>
 </table>
 </form>
+
+</div></div></body>
 </html>
--- application/views/tf_news_list.php
+++ application/views/tf_news_list.php
@@ -1,13 +1,33 @@
 <?php header("Content-Type:text/html;charset=utf-8");?>
 <html>
+<head>
+<link rel='stylesheet' media="screen" type='text/css' href='/CodeIgniter/css/views.css' />
+</head>
 <body>
-<a href="/CodeIgniter/index.php/Tf_news_c/insertView">添加新闻</a><br />
+<div id="bigBox">
+	<div id="left">
+        <ul>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/newsList">首页</a></li>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/newsList">新闻列表</a></li>
+        <li><a href="/CodeIgniter/index.php/Tf_news_c/insertView">添加新闻</a></li>
+        </ul>
+    </div>
+    <div id="right">
+    
+
+<table>
 <?php
 for($i=0;$news[$i];$i++){
-	echo $news[$i]->title;
-	echo "<a href=/CodeIgniter/index.php/Tf_news_c/del?id=".$news[$i]->id.">删除|</a>";
-	echo "<a href=/CodeIgniter/index.php/Tf_news_c/updateView?id=".$news[$i]->id.">编辑</a><br />";
+	echo "<tr>";
+	echo "<td>".$news[$i]->title."<td>";
+	echo "<td><a href=/CodeIgniter/index.php/Tf_news_c/del?id=".$news[$i]->id.">删除|</a></td>";
+	echo "<td><a href=/CodeIgniter/index.php/Tf_news_c/updateView?id=".$news[$i]->id.">编辑</a><br /></td>";
+	echo "</tr>";
 }
 ?>
+</table>
+
 </body>
+
+</div></div></body>
 </html>
